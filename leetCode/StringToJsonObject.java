d7901d7e9bd2658ecba51ed82bc57aa7044967e6
Ajay Prajapati <apprajapati@MacBook-Pro.local>
2017-05-21 17:39:54 -0400

    added string to json array object problm

---
+++ leetCode/StringToJsonObject.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leetCode;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.stream.JsonReader;
+import jdk.nashorn.internal.parser.JSONParser;
+
+/**
+ *
+ * @author apprajapati
+ */
+public class StringToJsonObject {
+
+    public static void main(String[] args) {
+
+        
+        String jsonStr = "[{\"No\":\"1\",\"Name\":\"ABC\"},{\"No\":\"2\",\"Name\":\"PQR\"},{\"No\":\"3\",\"Name\":\"XYZ\"}]";
+
+        JsonParser parse = new JsonParser();
+        JsonElement element = parse.parse(jsonStr); //Need to Add Gson library to use this.
+        
+       
+        System.out.println(element.toString());
+       
+  
+    }
+
+    public class JsonObject {
+
+        String id;
+        String name;
+        String salary;
+
+    }
+
+}
